79a94d618e881eaf1b9645d0322f3a08797f228f
No.5972 <wujiuqier@foxmail.com>
2021-03-26 10:36:54 +0800

    touhou

--- TouhouDemo/scenes/main.js
+++ TouhouDemo/scenes/main.js
@@ -1,269 +1,58 @@
-	const BULLET_SPEED = 800;
-  const ENEMY_BULLET_SPEED = 100;
-	const ENEMY_SPEED = 60;
-	const PLAYER_SPEED = 120;
-  const INITIAL_POWERUP_POINT_SPEED = -60;
-  const INITIAL_POWERUP_POWER_SPEED = -50;
+    add([
+      sprite("title00"),
+      scale(width() / 640, height() / 480),
+		  origin("topleft"),
+    ]);
 
-  let isFiring = false;
-  let power = 1;
-  let position = 0;
-  let life = 3;
-  let isRespawning = false;
+    play("th06_01");
+    
+    add([
+      pos(width() / 2, 40),
+      text("Touhou Akamakyou", 32),
+      color(1, 0, 0)
+    ]);
 
-  gravity(200);
+    add([
+      pos(width() / 2, 80),
+      text("V3.1"),
+      color(1, 0, 0)
+    ]);
 
-  const enemyALocation = [ 9, 12, 13, 14, 22, 23, 24 ];
-  const enemyBLocation = [32, 34, 36, 38, 40, 52, 54, 56, 58, 60];
-  const enemyCLocation = [31, 33, 35, 37, 39, 51, 53, 55, 57, 59];
-  const enemyDLocation = [10, 72, 74, 76, 78, 80, 92, 94, 96, 98, 100];
-  
-  play("th06_02");
+    add([
+      pos(width() / 2, height() / 2 + 120),
+      text("Enter: Start")
+    ]);
 
-	const player = add([
-		sprite("player01"),
-		pos(width() / 2, height() - 15),
-		scale(1),
-    "player"
-	]);
+    add([
+      pos(width() / 2, height() / 2 + 130),
+      text("Arrow Keys (Hold): Move")
+    ]);
 
-	keyDown("left", () => {
-		player.move(-PLAYER_SPEED, 0);
-	});
+    add([
+      pos(width() / 2, height() / 2 + 140),
+      text("Z (Hold): Fire")
+    ]);
 
-	keyDown("right", () => {
-		player.move(PLAYER_SPEED, 0);
-	});
+    add([
+      pos(width() / 2, height() / 2 + 150),
+      text("X: Bomb")
+    ]);
 
-  keyDown("up", () => {
-		player.move(0, -PLAYER_SPEED);
-	});
+    add([
+      pos(width() / 2, height() / 2 + 160),
+      text("Shift (Hold): Slow down and concentrate fire")
+    ]);
 
-  keyDown("down", () => {
-		player.move(0, PLAYER_SPEED);
-	});
+    add([
+      pos(width() / 2, height() / 2 + 170),
+      text("@2021 No.5972 . MIT License.")
+    ]);
 
-  keyDown("z", () => { isFiring = true; });
+    add([
+      pos(width() / 2, height() / 2 + 185),
+      text("This project is of 2ndary creation based on Touhou Koumakyou. \nCopyright of the material belongs to the original author.")
+    ]);
 
-  keyRelease("z", () => { isFiring = false; });
-
-/*
-	keyPress("space", () => {
-		add([
-			sprite("bullet00"),
-			pos(player.pos),
-			// strings here means a tag
-			"bullet",
-		]);
-	});
-*/
-	// run this callback every frame for all objects with tag "bullet"
-	action("bullet", (b) => {
-		b.move(Math.cos(b.direction) * BULLET_SPEED, -Math.sin(b.direction) * BULLET_SPEED);
-		// remove the bullet if it's out of the scene for performance
-		if (b.pos.y < 0) {
-			destroy(b);
-		}
-	});
-
-	function spawnEnemyA() { return add([	sprite("enemy00"),	pos(rand(0, width()), 0), "enemyA", ]);}
-  function spawnEnemyB() { return add([	sprite("enemy00"),	pos(0, 48), "enemyB", ]);}
-  function spawnEnemyC() { return add([	sprite("enemy00"),	pos(width(), 48), "enemyC", ]);}
-  function spawnEnemyD() { return add([	sprite("enemy01"),	pos(rand(0, width()), 0), "enemyD", {
-    time: 0
-  }]);}
-
-	const score = add([
-		pos(width() - 120, 12),
-		text("Score: 0"),
-		// all objects defaults origin to center, we want score text to be top left
-		origin("topleft"),
-		// plain objects becomes fields of score
-		{
-			value: 0,
-		},
-	]);
-
-  const powerText = add([
-    pos(width() - 120, 20),
-    text("Power: 1.0"),
-    origin("topleft"),
-    {}
-  ])
-
-  const lifeText = add([
-    pos(width() - 120, 28),
-    text("Life : 3"),
-    origin("topleft"),
-    {}
-  ])
-
-  function defeatAnEnemy(b, e) {
-		destroy(b);
-		destroy(e);
-		score.value += 1;
-		score.text = "Score: " + score.value;
-
-    let rand = Math.random();
-    if (rand > 0.6) {
-      add([
-        sprite("point"),
-        pos(e.pos),
-        body(),
-        // strings here means a tag
-        "powerup-point",
-      ]);
-    }
-
-    if (rand > 0.1 && rand <= 0.4) {
-      add([
-        sprite("power"),
-        pos(e.pos),
-        body(),
-        // strings here means a tag
-        "powerup-power",
-      ]);
-    }
-  }
-
-	// if a "bullet" and a "enemy" collides, remove both of them
-	collides("bullet", "enemyA", (b, e) => { defeatAnEnemy(b, e); });
-  collides("bullet", "enemyB", (b, e) => { defeatAnEnemy(b, e); });
-  collides("bullet", "enemyC", (b, e) => { defeatAnEnemy(b, e); });
-  collides("bullet", "enemyD", (b, e) => { defeatAnEnemy(b, e); });
-
-
-  collides("player", "powerup-point", (p, pp) => {
-    let getScore = parseInt(100 + height() - p.pos.y);
-    if (getScore > 800) {
-      getScore = 800;
-    }
-    score.value += getScore;
-    score.text = "Score: " + score.value;
-    destroy(pp);
-  });
-
-  collides("player", "powerup-power", (p, pp) => {
-    if (power < 10) {
-      //power+=2.3;
-      power+=0.3;
-      powerText.text = "Power: " + power.toFixed(1);
-      if (power > 10) {
-        power = 10;
-        powerText.text = "Power: MAX";
-      }
-      
-    }
-    destroy(pp);
-  });
-
-  function playerHit(p, e) {
-    isRespawning = true;
-    life--;
-    lifeText.text = "Life : " + life;
-    if (life < 0) {
-      go("gameover", score);
-      return;
-    }
-    destroy(p);
-    destroy(e);
-    wait(2, () => {
-      player.pos.x = width() / 2;
-      player.pos.y = height() - 15;
-      add(player);
-      isRespawning = false;
-    });
-  }
-
-  collides("player", "enemyBullet1", (e, eb) => { playerHit(e, eb); });
-  collides("player", "enemyA", (e, eb) => { playerHit(e, eb); });
-  collides("player", "enemyB", (e, eb) => { playerHit(e, eb); });
-  collides("player", "enemyC", (e, eb) => { playerHit(e, eb); });
-  collides("player", "enemyD", (e, eb) => { playerHit(e, eb); });
-
-	action("enemyA", (e) => {
-		e.move(0, ENEMY_SPEED);
-		if (e.pos.y > height()) {
-			destroy(e);
-		}
-	});
-
-  action("enemyB", (e) => {
-		e.move(ENEMY_SPEED, 0);
-		if (e.pos.x > width()) {
-			destroy(e);
-		}
-	});
-
-  action("enemyC", (e) => {
-		e.move(-ENEMY_SPEED, 0);
-		if (e.pos.x < 0) {
-			destroy(e);
-		}
-	});
-
-  action("enemyD", (e) => {
-		e.move(0, ENEMY_SPEED);
-    e.time++;
-    if (e.time % 100 == 0) { // enemy shoot
-      let randDirection = rand(0, Math.PI * 2);
-      for (let i = 0; i < 6; i ++) {
-        add([	sprite("eb1"),	pos(e.pos.x, e.pos.y), "enemyBullet1", {
-          direction: randDirection + ((Math.PI / 3) * i)
-        }]);
-      }
-    }
-		if (e.pos.y > height()) {
-			destroy(e);
-		}
-	});
-
-  action("powerup-point", (pp) => {
-    let currentSpeed = INITIAL_POWERUP_POINT_SPEED;
-    pp.move(0, currentSpeed++);
-    if (pp.pos > height()) {
-      destroy(pp);
-    }
-  });
-
-  action("powerup-power", (pp) => {
-    let currentSpeed = INITIAL_POWERUP_POWER_SPEED;
-    pp.move(0, currentSpeed++);
-    if (pp.pos > height()) {
-      destroy(pp);
-    }
-  });
-
-  action("enemyBullet1", (eb) => {
-    let currentSpeed = ENEMY_BULLET_SPEED;
-    eb.move(Math.cos(eb.direction) * currentSpeed, Math.sin(eb.direction) * currentSpeed);
-    if (eb.pos.x < 0 || eb.pos.x > width() || eb.pos.y < 0 || eb.pos.y > height()) {
-      destroy(eb);
-    }
-  })
-
-	// spawn an enemy every 1 second
-	loop(0.3, () => {
-    position++;
-    if (enemyALocation.lastIndexOf(position) != -1) spawnEnemyA();
-    if (enemyBLocation.lastIndexOf(position) != -1) spawnEnemyB();
-    if (enemyCLocation.lastIndexOf(position) != -1) spawnEnemyC();
-    if (enemyDLocation.lastIndexOf(position) != -1) spawnEnemyD();
-  });
-
-  loop(0.05, () => {
-    if (isFiring && !isRespawning) {
-      console.log("fire")
-      for (let i = 0; i < parseInt(power); i++) {
-        add([
-          sprite("bullet00"),
-          pos(player.pos),
-          // strings here means a tag
-          "bullet",
-          {
-            direction: Math.PI / 2 + (0.05 * parseInt(i + 1 / 2) * Math.pow(-1, i))
-          }
-        ]);
-      }
-    } 
-  });
+    keyPress("enter", () => {
+      go("level1", {});
+    })
