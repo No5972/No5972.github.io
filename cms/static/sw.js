83488ff90762221e577e04c09351f26609a0cc26
No5972 <wujiuqier@foxmail.com>
2022-06-03 11:58:40 +0000

    Update file(s) "/." from "No5972/cms"

--- cms/static/sw.js
+++ cms/static/sw.js
@@ -1,4 +1,4 @@
-<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/cms/assets/css/APlayer.min.css"><script src="/cms/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
+importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
  
 if (workbox) {
     workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' });
@@ -10,4 +10,4 @@ if (workbox) {
     workbox.routing.registerRoute(new RegExp('.*.html'), workbox.strategies.networkFirst());
  
     workbox.routing.registerRoute(new RegExp('.*.(?:js|css|jpg|png|gif)'), workbox.strategies.staleWhileRevalidate());
-}<link rel="stylesheet" href="/cms/css/spoiler.css" type="text/css"><script src="/cms/js/spoiler.js" type="text/javascript" async></script>
+}
